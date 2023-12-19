0bbf5e5f65460626a70864c072db79ff8a7e567f
DHRUVPATEL010 <128308791+DHRUVPATEL010@users.noreply.github.com>
2023-12-19 17:00:04 +0530

    final commit

--- Controllers/LeadsController.cs
+++ Controllers/LeadsController.cs
@@ -16,5 +16,94 @@ namespace CRMLeads.Controllers
 
             return View(leads);
         }
+
+        public IActionResult EditLead(int Id)
+        {
+            LeadsEntity leads = new LeadsEntity();
+
+            LeadRepository leadRepository = new LeadRepository();
+
+            leads = leadRepository.GetLeadById(Id);
+
+            return View(leads);
+        }   
+
+        public IActionResult EditLeadDetails(int Id , LeadsEntity leadDetails)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    LeadRepository _DbLead = new LeadRepository();
+                    if (_DbLead.EditLeadDetails(Id , leadDetails))
+                    {
+                        return RedirectToAction("Index");
+                    }
+                }
+                return View();
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult AddLead()
+        {
+            return View();
+        }
+
+        public ActionResult DeleteLead(int Id)
+        {
+            LeadsEntity leads = new LeadsEntity();
+
+            LeadRepository leadRepository = new LeadRepository();
+
+            leads = leadRepository.GetLeadById(Id);
+
+            return View(leads); 
+
+        }
+
+        public IActionResult DeleteLeadDetails(int Id, LeadsEntity leadDetails)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    LeadRepository _DbLead = new LeadRepository();
+                    if (_DbLead.DeleteLeadDetails(Id, leadDetails))
+                    {
+                        return RedirectToAction("Index");
+                    }
+                }
+                return View();
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+
+        public ActionResult AddNewLead(LeadsEntity LeadDetails)
+        {
+            try
+            {
+                if(ModelState.IsValid)
+                {
+                    LeadRepository _DbLead = new LeadRepository();
+                    if(_DbLead.AddLead(LeadDetails))
+                    {
+                        return RedirectToAction("Index");
+                    }
+                }
+                return View();
+            }
+            catch
+            {
+                return View();
+            }
+        }
     }
 }
--- Data/LeadRepository.cs
+++ Data/LeadRepository.cs
@@ -11,7 +11,6 @@ namespace CRMLeads.Data
         {
             string connstr = "server=acceptagreement.com ; database=testDB_Jigar ; User Id=sa;Password=Fall1130836!";
 
-
                 _connection = new SqlConnection(connstr);
 
         }
@@ -47,6 +46,115 @@ namespace CRMLeads.Data
             return leadsEntities;
 
 
+        }
+
+        public LeadsEntity GetLeadById(int Id)
+        {
+            LeadsEntity leadsEntities = new LeadsEntity();
+
+            SqlCommand cmd = new SqlCommand("GetLeadDetailsById", _connection);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+            SqlParameter param;
+            cmd.Parameters.Add(new SqlParameter("@Id", Id));
+
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(cmd);
+
+            DataTable dt = new DataTable();
+
+            dataAdapter.Fill(dt);
+
+            foreach (DataRow dr in dt.Rows)
+            {
+                leadsEntities = new LeadsEntity
+                {
+                    Id = Convert.ToInt32(dr["id"]),
+                    LeadDate = Convert.ToDateTime(dr["LeadDate"]),
+                    Name = dr["name"].ToString(),
+                    EmailAddress = dr["EmailAddress"].ToString(),
+                    Mobile = dr["Mobile"].ToString(),
+                    LeadSource = dr["LeadSource"].ToString(),
+                    LeadStatus = dr["LeadStatus"].ToString(),
+                    NextFollowUpDate = Convert.ToDateTime(dr["NextFollowUpDate"]),
+
+                };
+            }
+
+            return leadsEntities;
+
+
+        }
+
+        public bool AddLead(LeadsEntity leads)
+         {
+            SqlCommand cmd = new SqlCommand("AddLead",_connection);
+            cmd.CommandType= System.Data.CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@LeadDate", leads.LeadDate);
+            cmd.Parameters.AddWithValue("@Name", leads.Name);
+            cmd.Parameters.AddWithValue("@EmailAddress", leads.EmailAddress);
+            cmd.Parameters.AddWithValue("@Mobile", leads.Mobile);
+            cmd.Parameters.AddWithValue("@LeadSource", leads.LeadSource);
+            cmd.Parameters.AddWithValue("@LeadStatus", leads.LeadStatus);
+            cmd.Parameters.AddWithValue("@NextFollowUpDate", leads.NextFollowUpDate);
+
+            _connection.Open();
+
+            int i = cmd.ExecuteNonQuery(); 
+            _connection.Close();
+
+            if (i >= 1)
+            {
+                return true;
+            }
+            else { return false; }
+        }
+
+        public bool EditLeadDetails(int Id , LeadsEntity leads)
+        {
+            SqlCommand cmd = new SqlCommand("EditLead", _connection);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue ("@Id", leads.Id);
+            cmd.Parameters.AddWithValue("@LeadDate", leads.LeadDate);
+            cmd.Parameters.AddWithValue("@Name", leads.Name);
+            cmd.Parameters.AddWithValue("@EmailAddress", leads.EmailAddress);
+            cmd.Parameters.AddWithValue("@Mobile", leads.Mobile);
+            cmd.Parameters.AddWithValue("@LeadSource", leads.LeadSource);
+            cmd.Parameters.AddWithValue("@LeadStatus", leads.LeadStatus);
+            cmd.Parameters.AddWithValue("@NextFollowUpDate", leads.NextFollowUpDate);
+
+            _connection.Open();
+
+            int i = cmd.ExecuteNonQuery();
+            _connection.Close();
+
+            if (i >= 1)
+            {
+                return true;
+            }
+            else { return false; }
+        }
+
+
+        public bool DeleteLeadDetails(int Id, LeadsEntity leads)
+        {
+            SqlCommand cmd = new SqlCommand("DeleteLeadDetails", _connection);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@Id", leads.Id);
+           
+
+            _connection.Open();
+
+            int i = cmd.ExecuteNonQuery();
+            _connection.Close();
+
+            if (i >= 1)
+            {
+                return true;
+            }
+            else { return false; }
         }
     }
 }
